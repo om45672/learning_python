6ab4bbf6f719a069972822374a881def1087c45e
Om Patel <patelom927@gmail.com>
2026-06-06 18:56:28 +0530

    fix chapter_2 location

---
+++ 02_datatypes/chapter_2.py
@@ -0,0 +1,6 @@
+spice_mix = set()
+print(f"Initial spice mix id: {id(spice_mix)}")
+spice_mix.add("Ginger")
+spice_mix.add("cardamom")
+print(f"Initial spice mix id: {id(spice_mix)}")
+print(spice_mix)
